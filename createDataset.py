8ca8c751a722d48e6b0a27d802f18cbcaa97e8dc
gimpei <gim.kobayashi@gmail.com>
2017-07-17 23:33:03 +0900

    upadte

---
+++ createDataset.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import math
+import random
+import pickle
+def makePureData():
+  w = open('pureData.txt', 'w')
+  ii = [i for i in range(999999)]
+  random.shuffle(ii)
+  for i in ii:
+    if i%15 == 0:
+      a = '%09d FizzBuzz'%i
+    elif i%3 == 0:
+      a = '%09d Fizz'%i
+    elif i%5 == 0:
+      a = '%09d Buzz'%i
+    else:
+      a = '%09d そのまま'%i
+    w.write('%s\n'%a)
+
+def mapToFeatsIndex():
+  f = open('pureData.txt')
+
+  feat_index = {}
+  for line in f: 
+    line = line.strip()
+    feats, ans = line.split()
+    for e, char in enumerate( reversed(feats) ):
+      uniq = 'digit:%d, char:%s'%(e, char)
+      if feat_index.get( uniq ) is None:
+        feat_index[uniq] = len( feat_index )
+  open('feat_index.pkl', 'wb').write( pickle.dumps( feat_index ) )
+
+def makeSVMFormat():
+  f = open('pureData.txt')
+
+  feat_index = pickle.loads( open('feat_index.pkl', 'rb').read() )
+  ans_index  = {'FizzBuzz':0, 'Fizz':1, 'Buzz':2, 'そのまま':3}  
+  svm = open('svm.fmt', 'w')
+  for line in f: 
+    line = line.strip()
+    feats, ans = line.split()
+    feats_buff = []
+    for e, char in enumerate( reversed(feats) ):
+      uniq = 'digit:%d, char:%s'%(e, char)
+      feats_buff.append( feat_index[uniq] )
+    txt = ' '.join( ['%d:1.0'%f for f in feats_buff] )
+    ans = ans_index[ans]
+    svm.write( '{} {}\n'.format(ans, txt) )
+
+if __name__ == '__main__':
+  if '--step1' in sys.argv:
+    makePureData()
+
+  if '--step2' in sys.argv:
+    mapToFeatsIndex()
+
+  if '--step3' in sys.argv:
+    makeSVMFormat()
